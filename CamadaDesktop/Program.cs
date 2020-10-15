b1ee1ab8784f40a13a7c5ba416328b8731e06215
Lucas Gomes <lucas.amocristo@gmail.com>
2020-10-14 22:32:11 -0300

    Terceira linha add Merge

--- CamadaDesktop/Program.cs
+++ CamadaDesktop/Program.cs
@@ -16,7 +16,7 @@ namespace CamadaDesktop
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmMenu());
+            Application.Run(new frmLogin());
         }
     }
 }
