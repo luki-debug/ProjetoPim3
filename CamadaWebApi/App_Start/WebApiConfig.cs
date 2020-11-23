f541890fdc8535a68da6f2c92274c8654afabfe1
vannessaaah <58231175+vannessaaah@users.noreply.github.com>
2020-11-22 21:58:22 -0300

    Habilando o CORS

--- CamadaWebApi/App_Start/WebApiConfig.cs
+++ CamadaWebApi/App_Start/WebApiConfig.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net.Http.Formatting;
 using System.Net.Http.Headers;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace CamadaWebApi
 {
@@ -12,6 +13,8 @@ namespace CamadaWebApi
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            var cors = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(cors);
 
             // Web API routes
             config.MapHttpAttributeRoutes();
